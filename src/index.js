331521fc6122178033b3c17bd0efd2c8997bd2ea
Gabriel Figueiredo <gabriel.devsistema@gmail.com>
2022-05-12 11:38:15 -0300

    Criado rotas

--- src/index.js
+++ src/index.js
@@ -1,8 +1,20 @@
-const express = require('express')
+import * as  express from 'express'
 const app = express()
 
-app.get('/', function (req, res) {
-  res.send('Hello World')
+app.get('/', (req, res) => {
+  res.send("GET")
+})
+
+app.post('/', (req, res) => {
+  res.status(201).send("Post")
+})
+
+app.delete('/', (req, res) => {
+  res.send("DELETE")
+})
+
+app.put('/', (req, res) => {
+  res.send("PUT")
 })
 
 app.listen(3000)
